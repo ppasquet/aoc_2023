57473bfbbef9cf066610db7d7c1fb74e62d25852
Pierre Pasquet <pasquet.pierre@icloud.com>
2023-12-12 16:31:26 -0500

    Day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,33 +1,77 @@
 advent_of_code::solution!(4);
-use std::collections::HashSet;
+use once_cell::sync::Lazy;
 use regex::Regex;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
-fn collect_numbers(number_section: &str, number_pattern: &Regex) -> HashSet<u32> {
-    return number_pattern.find_iter(number_section)
-        .filter_map(|number_match| number_match.as_str().parse::<u32>().ok())
-        .collect::<HashSet<u32>>();
+#[derive(Debug)]
+struct Card {
+    winning_numbers: HashSet<u32>,
+    drawn_numbers: HashSet<u32>,
+}
+
+fn build_card(card_details: &str) -> Option<(String, Card)> {
+    static RE: Lazy<Regex> = Lazy::new(|| Regex::new(
+            r"Card\s+(?<card_id>\d+):\s(?<winning_nb>[\d\s]*)\s\|\s(?<drawn_nb>[\d\s]*)$").unwrap());
+    match RE.captures(card_details) {
+        Some(caps) => {
+            let winning_numbers = caps["winning_nb"].split_terminator(" ")
+                .filter_map(|nb| nb.parse::<u32>().ok()).collect::<HashSet<u32>>();
+            let drawn_numbers = caps["drawn_nb"].split_terminator(" ")
+                .filter_map(|nb| nb.parse::<u32>().ok()).collect::<HashSet<u32>>();
+            Some((String::from(&caps["card_id"]), Card { winning_numbers, drawn_numbers }))
+        }
+        None => None
+    }
+}
+
+fn score_card_one(card: &Card) -> u32 {
+    let common_numbers_count =
+        u32::try_from(card.drawn_numbers.intersection(&card.winning_numbers).count()).ok();
+    match common_numbers_count {
+        None => 0,
+        Some(0) => 0,
+        Some(k) => u32::pow(2, k - 1),
+    }
+}
+
+fn score_card_two(card: &Card) -> u32 {
+    let common_numbers_count =
+        u32::try_from(card.drawn_numbers.intersection(&card.winning_numbers).count()).ok();
+    match common_numbers_count {
+        Some(k) => k,
+        None => 0,
+    }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let number_pattern = Regex::new(r"\d+").unwrap();
-    let res = input.split_terminator("\n").map(|card| {
-        let mut numbers_sections = card.split(":").last().unwrap_or("").split("|");
-        let winning_numbers_section = &numbers_sections.next().unwrap_or("");
-        let drawn_numbers_section = &numbers_sections.last().unwrap_or("");
-        let winning_numbers = collect_numbers(&winning_numbers_section, &number_pattern);
-        let drawn_numbers = collect_numbers(&drawn_numbers_section, &number_pattern);
-        let common_numbers_count = u32::try_from(drawn_numbers.intersection(&winning_numbers).count()).ok();
-        match common_numbers_count {
-            None => 0,
-            Some(0) => 0,
-            Some(k) => u32::pow(2, k - 1),
-        }
-    }).sum::<u32>();
+    let res = input
+        .split_terminator("\n")
+        .map(|row| {
+            match build_card(row) {
+                Some((_, card)) => score_card_one(&card),
+                _ => 0,
+            }
+        })
+        .sum::<u32>();
     Some(res)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let rows = input.split_terminator("\n");
+    let mut card_counts = vec![1; rows.clone().collect::<Vec<_>>().len()];
+    for (i, row) in rows.enumerate() {
+        let card_matches = match build_card(row) {
+            Some((_, card)) => {
+                score_card_two(&card)
+            },
+            None => 0,
+        };
+        for j in i + 1..i + 1 + card_matches as usize {
+            card_counts[j] += card_counts[i];
+        }
+    }
+    Some(card_counts.iter().sum::<u32>())
 }
 
 #[cfg(test)]
@@ -37,12 +81,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(13));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(30));
     }
 }
